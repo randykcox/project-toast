8a75d12bcabf395c1343a9f7be2c31743cd2e14b
Randy Cox <randy@kenzie.academy>
2024-03-29 15:03:29 -0400

    Show toast preview in playground and make toast dismissable. (Exercise 2 complete)

--- src/components/ToastPlayground/ToastPlayground.js
+++ src/components/ToastPlayground/ToastPlayground.js
@@ -1,6 +1,7 @@
 import React from 'react';
 
 import Button from '../Button';
+import Toast from '../Toast'
 
 import styles from './ToastPlayground.module.css';
 
@@ -9,6 +10,7 @@ const VARIANT_OPTIONS = ['notice', 'warning', 'success', 'error'];
 function ToastPlayground() {
   const [message, setMessage] = React.useState('')
   const [variant, setVariant ] = React.useState('notice')
+  const [toastVisible, setToastVisible] = React.useState(false)
 
   return (
     <div className={styles.wrapper}>
@@ -16,6 +18,12 @@ function ToastPlayground() {
         <img alt="Cute toast mascot" src="/toast.png" />
         <h1>Toast Playground</h1>
       </header>
+
+      {toastVisible && 
+      <Toast visible={toastVisible} setVisible={setToastVisible} variant={variant}>
+        {message}
+      </Toast>
+}
 
       <div className={styles.controlsWrapper}>
         <div className={styles.row}>
@@ -58,7 +66,11 @@ function ToastPlayground() {
         <div className={styles.row}>
           <div className={styles.label} />
           <div className={`${styles.inputWrapper} ${styles.radioWrapper}`}>
-            <Button>Pop Toast!</Button>
+            <Button
+            onClick={event=>{
+              setToastVisible(true)
+            }}
+            >Pop Toast!</Button>
           </div>
         </div>
       </div>
